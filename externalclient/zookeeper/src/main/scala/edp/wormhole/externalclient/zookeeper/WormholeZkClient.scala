c82ced282ec0048012700f28cca272d1831820e8
bjwangguangliang <bjwangguangliang@corp.netease.com>
2018-08-23 20:41:47 +0800

    kafkainput

--- externalclient/zookeeper/src/main/scala/edp/wormhole/externalclient/zookeeper/WormholeZkClient.scala
+++ externalclient/zookeeper/src/main/scala/edp/wormhole/externalclient/zookeeper/WormholeZkClient.scala
@@ -26,25 +26,44 @@ import org.apache.curator.framework.recipes.cache._
 import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
 import org.apache.curator.retry.ExponentialBackoffRetry
 import org.apache.zookeeper.data.Stat
-
+/**
+  * zookeeper 操作类
+  * Curator是Netflix公司一个开源的zookeeper客户端，
+  * 在原生API接口上进行了包装，解决了很多ZooKeeper客户端非常底层的细节开发。
+  * 同时内部实现了诸如Session超时重连，Watcher反复注册等功能，实现了Fluent风格的API接口，是使用最广泛的zookeeper客户端之一。
+  *
+  * 监控目录为 val watchPath = WormholeConstants.CheckpointRootPath + config.spark_config.stream_id + flowRelativePath
+  *           val udfPath = WormholeConstants.CheckpointRootPath + config.spark_config.stream_id + udfRelativePath
+  * 其中  WormholeConstants.CheckpointRootPath = "/wormhole/"
+  *       flowRelativePath = "/flow"
+  *       udfRelativePath = "/udf"
+  *
+  * 在zk下主目录为/wormhole，在该主目录下，以stream_id为目录继续划分/wormhole/${stream_id}/[offset, application_1530860253617_0048, flow, udf]
+  *
+  *
+  *
+  */
 object WormholeZkClient {
 
   @volatile var zkClient: CuratorFramework = null
 
   //  lazy val zookeeperPath:String = null
+  // 单例设计模式得到CuratorFramework
   def getZkClient(zkAddress: String): CuratorFramework = {
     if (zkClient == null) {
       synchronized {
         if (zkClient == null) {
-          val retryPolicy = new ExponentialBackoffRetry(1000, 3)
+          val retryPolicy = new ExponentialBackoffRetry(1000, 3)  // 初试时间为1s 重试3次
           zkClient = CuratorFrameworkFactory.newClient(getZkAddress(zkAddress), retryPolicy)
-          zkClient.start()
+          zkClient.start() // 开启连接
         }
       }
     }
     zkClient
   }
-
+  /**
+    * 关闭连接
+    */
   def closeZkClient(): Unit ={
     try{
       if (zkClient != null) {
@@ -54,7 +73,12 @@ object WormholeZkClient {
       case e:Throwable=>println("zkClient.close error")
     }
   }
-
+  /**
+    * 全局计数器
+    * @param zkAddress
+    * @param path
+    * @return
+    */
   def getNextAtomicIncrement(zkAddress: String, path: String): Long = {
     // todo if it failed? try catch not work
     val atomicLong = new DistributedAtomicLong(getZkClient(zkAddress), path, new ExponentialBackoffRetry(1000, 3))
@@ -65,6 +89,14 @@ object WormholeZkClient {
     newValue.postValue()
   }
 
+  /**
+    * 监控子节点状态
+    * @param zkAddress
+    * @param path
+    * @param add
+    * @param remove
+    * @param update
+    */
   def setPathChildrenCacheListener(zkAddress: String, path: String, add: (String, String, Long) => Unit, remove: String => Unit, update: (String, String, Long) => Unit): Unit = {
     val pathChildrenCache = new PathChildrenCache(getZkClient(zkAddress), getPath(zkAddress, path), true)
     pathChildrenCache.getListenable.addListener(new PathChildrenCacheListener() {
@@ -93,6 +125,14 @@ object WormholeZkClient {
 
   }
 
+  /**
+    * 监控节点状态
+    * @param zkAddress
+    * @param path
+    * @param add
+    * @param remove
+    * @param update
+    */
   def setNodeCacheListener(zkAddress: String, path: String, add: (String, String, Long) => Unit, remove: String => Unit, update: (String, String, Long) => Unit): Unit = {
     val nodeCache = new NodeCache(getZkClient(zkAddress), getPath(zkAddress, path))
     nodeCache.getListenable.addListener(new NodeCacheListener() {
@@ -109,6 +149,14 @@ object WormholeZkClient {
 
   }
 
+  /**
+    * 监控节点及子节点的状态
+    * @param zkAddress
+    * @param path
+    * @param add
+    * @param remove
+    * @param update
+    */
   //NOT USE YET
   def setTreeCacheListener(zkAddress: String, path: String, add: (String, String, Long) => Unit, remove: String => Unit, update: (String, Array[String], Long) => Unit): Unit = {
     val treeCache = new TreeCache(getZkClient(zkAddress), getPath(zkAddress, path))
@@ -138,7 +186,13 @@ object WormholeZkClient {
 
   }
 
-
+  /**
+    * 创建或更新节点值
+    * 如果该节点存在则更新，否则创建并赋值
+    * @param zkAddress
+    * @param path
+    * @param byte
+    */
   def createAndSetData(zkAddress: String, path: String, byte: Array[Byte]): Unit = {
     if (!checkExist(zkAddress, path)) {
       getZkClient(zkAddress).create().creatingParentsIfNeeded().forPath(getPath(zkAddress, path), byte)
@@ -147,37 +201,76 @@ object WormholeZkClient {
     }
   }
 
+  /**
+    * 创建节点目录
+    * @param zkAddress
+    * @param path
+    */
   def createPath(zkAddress: String, path: String): Unit = {
     if (!checkExist(zkAddress, path)) {
       getZkClient(zkAddress).create().creatingParentsIfNeeded().forPath(getPath(zkAddress, path))
     }
   }
 
+  /**
+    * 得到当前目录下的所有节点（仅限当前目录）
+    * @param zkAddress
+    * @param path
+    * @return
+    */
   def getChildren(zkAddress: String, path: String): Seq[String] = {
     val list = getZkClient(zkAddress).getChildren.forPath(getPath(zkAddress, path))
     import scala.collection.JavaConverters._
     list.asScala
   }
 
+  /**
+    * 给节点赋值
+    * @param zkAddress
+    * @param path
+    * @param payload
+    * @return
+    */
   def setData(zkAddress: String, path: String, payload: Array[Byte]): Stat = {
     getZkClient(zkAddress).setData().forPath(getPath(zkAddress, path), payload)
   }
 
+  /**
+    * 得到该节点的值
+    * @param zkAddress
+    * @param path
+    * @return
+    */
   def getData(zkAddress: String, path: String): Array[Byte] = {
     getZkClient(zkAddress).getData.forPath(getPath(zkAddress, path))
   }
 
-
+  /**
+    * 删除目录、节点
+    * @param zkAddress
+    * @param path
+    */
   def delete(zkAddress: String, path: String): Unit = {
     if (checkExist(zkAddress, path)) {
       getZkClient(zkAddress).delete().deletingChildrenIfNeeded().forPath(getPath(zkAddress, path))
     }
   }
 
+  /**
+    * 判断是否存在
+    * @param zkAddress
+    * @param path
+    * @return
+    */
   def checkExist(zkAddress: String, path: String): Boolean = {
     getZkClient(zkAddress).checkExists().forPath(getPath(zkAddress, path)) != null
   }
 
+  /**
+    * ip1/2181,ip2/2181,ip3/2181,ip4/2181 转换成 ip1,ip2,ip3,ip4
+    * @param zkAddress
+    * @return
+    */
   def getZkAddress(zkAddress: String): String = zkAddress.split(",").map(_.split("\\/")(0)).mkString(",")
 
   def getPath(zkAddress: String, path: String): String = {
